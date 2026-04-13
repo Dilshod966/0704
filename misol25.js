5b60ef208a43e047d6092ee419422514f748655b
elbek20122012 <elbekrozmatov777@gmail.com>
2026-04-13 16:21:03 +0500

    ishladim man elbekman

---
+++ misol25.js
@@ -0,0 +1,11 @@
+let tt,x,a;
+
+
+
+x=1.28;
+a=4;
+
+
+tt=(Math.sqrt(x-1)+Math.sqrt(x+2)+Math.log10(Math.sqrt(a*x*x)+2))/(Math.sqrt(Math.sqrt(x+2)+Math.sqrt(x+24)+x*x*x*x*x))
+
+console.log(tt.toFixed(2))
