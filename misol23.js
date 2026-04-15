9ff4a64d02bced9afebc73a7f9fc5b70f652c3dd
Avezmetov143 <jahongiravezmetov6@gmail.com>
2026-04-15 16:23:18 +0500

    man ihlab boldim

---
+++ misol23.js
@@ -0,0 +1,12 @@
+let y2, a, b, c, d, x;
+
+
+a=3
+b=1
+c=
+d=
+x=
+y2=(ax*ax+bx+c)/(xa*xa*xa+a*a Math.pow(a,a-b))+Math.cos (Math.abs)  (a*x+b)/(c*x+d+2)
+
+
+console.log(y2.toFixed(2))
---
+++ misol37.js
@@ -0,0 +1,10 @@
+let a,b;
+
+a= 4;
+b=0;
+if(a<=b){
+    a=0
+}
+console.log(a,+   ""    +b)
+
+
