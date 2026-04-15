8887d8ac930b19f75a26a5f9faba05c11f483f95
davlatnazarova07 <rahimadavlatnazarova17@gmail.com>
2026-04-15 16:38:12 +0500

    ishladim

--- misol24.js
+++ misol24.js
@@ -4,4 +4,4 @@ b = 1;
 c = 1; 
 x = 0.33;
 W1 = 0.75 + (8.2*x*x+Math.sqrt(Math.abs(x*x*x+3*x) + Math.cos(x-2)) ) /(a/4 + b/3 + c/2 +1)
-console.log(W1.toFixed(2))
+console.log(W1.toFixed(2)) 
---
+++ misol31.js
@@ -0,0 +1,13 @@
+let x, y, j1, j2;
+x = 0;
+y = 1;
+
+if (x < y) {
+  j1 = (x + y) / 2;
+  j2 = 2 * x * 2 * y;
+} else {
+  j1 = (x + y) / 2;
+  j2 = 2 * x * 2 * y;
+}
+
+console.log(j1.toFixed(1) + " " + j2.toFixed(1));
