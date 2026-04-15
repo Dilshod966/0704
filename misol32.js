4a8ff18b4973750ccbf2cbebf4a4936ea0cb66ac
yoqutjona <azadausupova60@gmail.com>
2026-04-15 15:50:15 +0500

    ishladim

---
+++ misol32.js
@@ -0,0 +1,23 @@
+let x, y, z;
+
+x = 3.96;
+y = 3.58;
+z = 2.83;
+
+if(x>y) {
+    if(x>z){
+        console.log('kattasi ' + x);
+    }
+    else {
+        console.log('kattasi' + z);
+    }
+}
+else {
+    if(y>z) {
+        console.log('kattasi ' + y);
+    }
+    else {
+        console.log('kattasi ' + z)
+    }
+}
+
